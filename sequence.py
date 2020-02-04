af07693dc7766720e155418616b4d17e692e0aa8
Qiu <qiuosier@gmail.com>
2020-02-04 13:53:35 -0500

    Refactor sequence.py

--- sequence.py
+++ sequence.py
@@ -1,5 +1,6 @@
 import numpy as np
 import editdistance
+import parasail
 
 
 COMPLEMENTS = {
@@ -22,48 +23,97 @@ COMPLEMENTS = {
 }
 
 
-# The levenshtein function is copied from
-# https://stackabuse.com/levenshtein-distance-and-text-similarity-in-python/
-def levenshtein(s1, s2):
-    size_x = len(s1) + 1
-    size_y = len(s2) + 1
-    matrix = np.zeros((size_x, size_y))
-    for x in range(size_x):
-        matrix[x, 0] = x
-    for y in range(size_y):
-        matrix[0, y] = y
+class Distance:
+    """Contains static functions for calculating distance between two sequences.
 
-    for x in range(1, size_x):
-        for y in range(1, size_y):
-            if s1[x - 1] == s2[y - 1]:
-                matrix[x, y] = min(
-                    matrix[x-1, y] + 1,
-                    matrix[x-1, y-1],
-                    matrix[x, y-1] + 1
-                )
+    All functions are static.
+    Each function returns a tuple of:
+        0: distance, or the number of mismatches
+        1: last matched position (0-indexed) in the second (reference) sequence.
+            -1 if there is no match at all.
+            None if this is not calculated.
+
+    """
+    # The levenshtein function is copied from
+    # https://stackabuse.com/levenshtein-distance-and-text-similarity-in-python/
+    @staticmethod
+    def levenshtein(s1, s2):
+        """This is a slow version of levenshtein distance.
+        Use edit_distance() for faster calculation.
+        """
+        size_x = len(s1) + 1
+        size_y = len(s2) + 1
+        matrix = np.zeros((size_x, size_y))
+        for x in range(size_x):
+            matrix[x, 0] = x
+        for y in range(size_y):
+            matrix[0, y] = y
+
+        for x in range(1, size_x):
+            for y in range(1, size_y):
+                if s1[x - 1] == s2[y - 1]:
+                    matrix[x, y] = min(
+                        matrix[x-1, y] + 1,
+                        matrix[x-1, y-1],
+                        matrix[x, y-1] + 1
+                    )
+                else:
+                    matrix[x, y] = min(
+                        matrix[x-1, y] + 1,
+                        matrix[x-1, y-1] + 1,
+                        matrix[x, y-1] + 1
+                    )
+        return matrix[size_x - 1, size_y - 1], None
+
+    @staticmethod
+    def edit_distance(s1, s2):
+        return editdistance.eval(s1, s2), None
+
+    @staticmethod
+    def hamming(s1, s2):
+        """Calculates the hamming distance of two sequences.
+        The two sequences should have the same length.
+        Otherwise, the longer one will be truncated (additional sequences are ignored).
+
+        Args:
+            s1:
+            s2:
+
+        Returns:
+
+        """
+        l1 = len(s1)
+        l2 = len(s2)
+        last_matched = -1
+        mismatch = 0
+        for i in range(min(l1, l2)):
+            c_1 = s1[i] if i < l1 else ""
+            c_2 = s2[i] if i < l2 else ""
+            if c_1 != c_2:
+                mismatch += 1
             else:
-                matrix[x, y] = min(
-                    matrix[x-1, y] + 1,
-                    matrix[x-1, y-1] + 1,
-                    matrix[x, y-1] + 1
-                )
-    return matrix[size_x - 1, size_y - 1]
+                last_matched = i
+        return mismatch, last_matched
 
+    dna_alignment_matrix = parasail.matrix_create("ACGT", 0, -1)
 
-def semi_global(s1, s2):
-    pass
+    @staticmethod
+    def dna_alignment(query, ref, algorithm='sg_de'):
+        """
 
+        Args:
+            query: query
+            ref: reference
+            algorithm: Function names in the algorithm table from parassail package.
+            See https://github.com/jeffdaily/parasail#standard-function-naming-convention for all function names
 
-def hamming(s1, s2):
-    l1 = len(s1)
-    l2 = len(s2)
-    mismatch = 0
-    for i in range(max(l1, l2)):
-        c_1 = s1[i] if i < l1 else ""
-        c_2 = s2[i] if i < l2 else ""
-        if c_1 != c_2:
-            mismatch += 1
-    return mismatch
+        Returns: Alignment results
+
+        """
+        func_name = "%s_stats" % algorithm
+        func = getattr(parasail, func_name)
+        results = func(query, ref, 1, 1, Distance.dna_alignment_matrix)
+        return results.score, results.end_ref
 
 
 class Sequence:
@@ -82,7 +132,11 @@ class Sequence:
     def reverse_complements(self):
         return self.complements[::-1]
 
-    def match(self, sequence):
+    def match(self, sequence, algorithm="hamming"):
         s1 = self.sequence.upper()
         s2 = sequence.upper()
-        return editdistance.eval(s1, s2)
+        if hasattr(Distance, algorithm):
+            func = getattr(Distance, algorithm)
+            return func(s1, s2)[0]
+        func = Distance.dna_alignment
+        return func(s1, s2, algorithm)[0]
